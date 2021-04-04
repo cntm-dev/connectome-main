c35cbb5e413bc9d1594164ad24dc94eae2495124
cntm-dev <cntmbusiness@gmail.com>
2021-04-05 04:09:01 +0800

    Init the crypto modules

--- crypto/keys.go
+++ crypto/keys.go
@@ -2,6 +2,10 @@ package crypto
 
 import (
 	"io"
+	"errors"
+	//"math/big"
+	"crypto/rand"
+	"crypto/ecdsa"
 )
 
 type PubKey ECPoint
@@ -13,4 +17,45 @@ func (e *PubKey) Serialize(w io.Writer) {
 func (ep *PubKey) EncodePoint(commpressed bool) []byte{
 	//TODO: EncodePoint
 	return nil
+}
+
+func NewPubKey(prikey []byte) *PubKey{
+       //TODO: NewPubKey
+       return nil
+}
+
+func GenPrivKey() []byte {
+	return nil
+}
+
+//FIXME, does the privkey need base58 encoding?
+//This generates a public & private key pair
+func GenKeyPair() ([]byte, PubKey, error) {
+	pubkey := new(PubKey)
+	privatekey := new(ecdsa.PrivateKey)
+	privatekey, err := ecdsa.GenerateKey(Crypto.curve, rand.Reader)
+	if err != nil {
+		return nil, *pubkey, errors.New("Generate key pair error")
+	}
+
+	privkey, err := privatekey.D.MarshalText()
+	pubkey.X = privatekey.PublicKey.X
+	pubkey.Y = privatekey.PublicKey.Y
+	return privkey, *pubkey, nil
+}
+
+func DecodePoint(encoded []byte) *PubKey{
+	//TODO: DecodePoint
+	return nil
+}
+
+type PubKeySlice []*PubKey
+
+func (p PubKeySlice) Len() int           { return len(p) }
+func (p PubKeySlice) Less(i, j int) bool {
+	//TODO:PubKeySlice Less
+	return false
+}
+func (p PubKeySlice) Swap(i, j int) {
+	//TODO:PubKeySlice Swap
 }
