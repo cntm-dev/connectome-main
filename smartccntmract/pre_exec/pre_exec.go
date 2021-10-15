5cab7409276ccb24fc4758b349891bf4ac3adfc9
cntm-dev <cntmbusiness@gmail.com>
2021-10-15 15:17:31 +0800

    Fix Pre Execute nil pointer

--- smartccntmract/pre_exec/pre_exec.go
+++ smartccntmract/pre_exec/pre_exec.go
@@ -4,13 +4,15 @@ import (
 	"github.com/Ontology/smartccntmract/service"
 	"github.com/Ontology/vm/neovm"
 	"github.com/Ontology/vm/neovm/interfaces"
-	"github.com/Ontology/core/store/statestore"
 	"github.com/Ontology/smartccntmract/types"
 	"github.com/Ontology/core/store/ChainStore"
-	"github.com/Ontology/core/ledger"
 	"github.com/Ontology/smartccntmract/common"
+	"github.com/Ontology/core/ledger"
+	"github.com/Ontology/core/store/statestore"
 	. "github.com/Ontology/common"
 )
+
+var DefaultEventStore ChainStore.IEventStore
 
 func PreExec(code []byte, ccntmainer interfaces.ICodeCcntmainer) ([]interface{}, error) {
 	var (
@@ -18,7 +20,8 @@ func PreExec(code []byte, ccntmainer interfaces.ICodeCcntmainer) ([]interface{},
 		err error
 	)
 	crypto = new(neovm.ECDsaCrypto)
-	stateStore := ChainStore.NewStateStore(statestore.NewMemDatabase(), ledger.DefaultLedger.Store.(*ChainStore.ChainStore), nil, Uint256{})
+
+	stateStore := ChainStore.NewStateStore(statestore.NewMemDatabase(), ledger.DefaultLedger.Store.(*ChainStore.ChainStore), Uint256{})
 	stateMachine := service.NewStateMachine(stateStore, types.Application, nil)
 	se := neovm.NewExecutionEngine(ccntmainer, crypto, ChainStore.NewCacheCodeTable(stateStore), stateMachine)
 	se.LoadCode(code, false)
